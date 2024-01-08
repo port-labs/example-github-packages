eade6540b033602bb2928360cdbdd6faa2023e12
lordsarcastic <>
2024-01-08 18:38:35 +0100

    Feature: Implemented script to ingest github packages into Port

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
-# example-github-packages
+# Getting started
+In this example, you will create a blueprint for `githubPackage` that ingests Github Packages from your GitHub organization into Port. You will then use a Python script to make API calls to GitHub REST API to fetch the data from your account.
+
+## Blueprints
+Create the following blueprints in Port using the schemas:
+
+
+### Package
 Python Script for Ingesting Github Packages in Port
+
+```json
+{
+  "identifier": "githubPackage",
+  "description": "This blueprint represents a GitHub package in our software catalog",
+  "title": "Github Package",
+  "icon": "Github",
+  "schema": {
+    "properties": {
+      "packageType": {
+        "type": "string",
+        "title": "Package Type",
+        "enum": [
+          "npm",
+          "maven",
+          "rubygems",
+          "docker",
+          "nuget",
+          "container"
+        ]
+      },
+      "visibility": {
+        "type": "string",
+        "title": "Visibility",
+        "enum": [
+          "public",
+          "private",
+          "internal"
+        ]
+      },
+      "createdAt": {
+        "type": "string",
+        "title": "Created At",
+        "format": "date-time"
+      },
+      "link": {
+        "type": "string",
+        "title": "Link",
+        "format": "url"
+      },
+      "latestVersionTag": {
+        "title": "Latest Version Tags",
+        "type": "array"
+      },
+      "latestVersionCreatedAt": {
+        "type": "string",
+        "title": "Latest Version Created At",
+        "format": "date-time"
+      },
+      "latestVersionLink": {
+        "title": "Latest Version Link",
+        "type": "string",
+        "format": "url"
+      }
+    },
+    "required": []
+  },
+  "mirrorProperties": {},
+  "calculationProperties": {},
+  "relations": {}
+}
+```
+
+## Running the Python script
+First clone the repository and cd into the work directory with:
+```bash
+$ git clone https://github.com/port-labs/example-github-packages.git
+$ cd example-github-packages
+```
+
+Install the needed dependencies within the context of a virtual environment with:
+```bash
+$ virtualenv venv
+$ source venv/bin/activate
+$ pip3 install -r requirements.txt
+```
+
+To ingest your data, you need to populate some environment variables. You can do that by either duplicating the `.example.env` file and renaming the copy as `.env`, then edit the values as needed; or run the commands below in your terminal:
+
+```bash
+export PORT_CLIENT_ID=port_client_id
+export PORT_CLIENT_SECRET=port_client_secret
+export GITHUB_ACCESS_TOKEN=token
+export ORGANIZATION_NAME=organization_name
+```
+
+Then run the script with:
+```bash
+$ python app.py
+```
+
+Each variable required are:
+- PORT_CLIENT_ID: Port Client ID
+- PORT_CLIENT_SECRET: Port Client secret
+- GITHUB_ACCESS_TOKEN: Github access token
+- ORGANIZATION_NAME: Github organization name
+
+You can get the Github access token by following the instructions on the [authentication section](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-personal-access-token-classic)
---
+++ app.py
@@ -0,0 +1,171 @@
+import asyncio
+import logging
+import os
+from typing import Any
+
+import aiohttp
+import dotenv
+import requests
+
+dotenv.load_dotenv()
+
+logger = logging.getLogger(__name__)
+
+PORT_API_URL = "https://api.getport.io/v1"
+PORT_CLIENT_ID = os.getenv("PORT_CLIENT_ID")
+PORT_CLIENT_SECRET = os.getenv("PORT_CLIENT_SECRET")
+GITHUB_ACCESS_TOKEN = os.getenv("GITHUB_ACCESS_TOKEN")
+ORGANIZATION_NAME = os.getenv("ORGANIZATION_NAME")
+REPOSITORY_BLUEPRINT = "githubPackage"
+PACKAGE_TYPES = ["npm", "maven", "rubygems", "docker", "nuget", "container"]
+DEFAULT_PAGE_SIZE = 100
+DEFAULT_PAGE_NUMBER = 1
+
+## Get Port Access Token
+credentials = {"clientId": PORT_CLIENT_ID, "clientSecret": PORT_CLIENT_SECRET}
+
+token_response = requests.post(f"{PORT_API_URL}/auth/access_token", json=credentials)
+access_token = token_response.json()["accessToken"]
+
+# You can now use the value in access_token when making further requests
+headers = {"Authorization": f"Bearer {access_token}"}
+
+
+async def add_entity_to_port(
+    session: aiohttp.ClientSession, blueprint_id, entity_object
+):
+    """A function to create the passed entity in Port
+
+    Params
+    --------------
+    blueprint_id: str
+        The blueprint id to create the entity in Port
+
+    entity_object: dict
+        The entity to add in your Port catalog
+
+    Returns
+    --------------
+    response: dict
+        The response object after calling the webhook
+    """
+    logger.info(f"Adding entity to Port: {entity_object}")
+    response = await session.post(
+        (
+            f"{PORT_API_URL}/blueprints/"
+            f"{blueprint_id}/entities?upsert=true&merge=true"
+        ),
+        json=entity_object,
+        headers=headers,
+    )
+    if not response.ok:
+        logger.info("Ingesting {blueprint_id} entity to port failed, skipping...")
+    logger.info(f"Added entity to Port: {entity_object}")
+
+
+async def get_github_packages(
+    session: aiohttp.ClientSession, url: str, package_type: str
+) -> list[dict[str, Any]]:
+    """
+    A function to get all the packages for a given repo
+
+    Returns
+    --------------
+    packages: dict
+        The packages for the given repo
+    """
+    query_params = {
+        "package_type": package_type,
+        "per_page": DEFAULT_PAGE_SIZE,
+        "page": DEFAULT_PAGE_NUMBER,
+    }
+    logger.info(f"Getting packages")
+    page_size = DEFAULT_PAGE_SIZE
+
+    while page_size >= DEFAULT_PAGE_SIZE:
+        async with session.get(
+            f"{url}/packages?",
+            params=query_params,
+            headers={
+                "Authorization": f"Bearer {GITHUB_ACCESS_TOKEN}",
+                "Accept": "application/vnd.github+json",
+                "X-GitHub-Api-Version": "2022-11-28",
+            },
+        ) as response:
+            response.raise_for_status()
+            packages: list[dict[str, Any]] = await response.json()
+            page_size = len(packages)
+            query_params["page"] += 1
+            logger.info(f"Retrieved packages")
+            yield packages
+
+
+async def get_package_metadata(
+    session: aiohttp.ClientSession, package: dict[str, Any]
+) -> dict[str, Any]:
+    """
+    A function to get the metadata for a given package
+
+    Returns
+    --------------
+    metadata: dict
+        The metadata for the given package
+    """
+    logger.info(f"Getting package metadata")
+    async with session.get(
+        f"{package['url']}/versions",
+        headers={
+            "Authorization": f"Bearer {GITHUB_ACCESS_TOKEN}",
+            "Accept": "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28",
+        },
+    ) as response:
+        response.raise_for_status()
+        metadata: list[dict[str, Any]] = await response.json()
+        logger.info(f"Retrieved package metadata")
+        if not metadata:
+            return None
+        return metadata[0]
+
+
+async def ingest_package_into_port(
+    session: aiohttp.ClientSession,
+    package: dict[str, Any],
+    package_metadata: dict[str, Any],
+    package_type: str,
+):
+    package_blueprint = {
+        "identifier": str(package["id"]),
+        "title": package["name"],
+        "properties": {
+            "packageType": package_type,
+            "visibility": package["visibility"],
+            "createdAt": package["created_at"],
+            "link": package["html_url"],
+            "latestVersionTag": package_metadata["name"],
+            "latestVersionLink": package_metadata["html_url"],
+            "latestVersionCreatedAt": package_metadata["created_at"],
+        },
+    }
+
+    await add_entity_to_port(session, REPOSITORY_BLUEPRINT, package_blueprint)
+
+
+async def main():
+    logger.info("Starting Port integration")
+    url = "https://api.github.com/orgs/{ORGANIZATION_NAME}"
+    async with aiohttp.ClientSession() as session:
+        for package_type in PACKAGE_TYPES:
+            async for packages in get_github_packages(session, url, package_type):
+                for package in packages:
+                    package_metadata = await get_package_metadata(session, package)
+                    if not package_metadata:
+                        continue
+                    await ingest_package_into_port(
+                        session, package, package_metadata, package_type
+                    )
+    logger.info("Finished Port integration")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
